f4c6947ebb1d149292c78da0a533cbcf18cca6b5
khouloud137 <khouloudgouttai@gmail.com>
2024-02-04 14:22:05 +0100

    express

---
+++ app.js
@@ -0,0 +1,18 @@
+// const express = require("express");
+// const app = express();
+
+// app.get("/", (req, res) => {
+//   res.send("<p>  home page</p>");
+// });
+// app.get("/about", (req, res) => {
+//   res.send("<p>  about page</p>");
+// });
+//lIsten for requestes
+// app.listen(3000, (err) => {
+//   if (err) {
+    // console.log(err);
+//   } else {
+   
+// console.log("listening on port 3000");
+//   }
+// });
---
+++ server.js
@@ -0,0 +1,35 @@
+// load the things we need
+var express = require("express");
+var app = express();
+
+// set the view engine to ejs
+app.use(express.static("./public"));
+app.set("view engine", "ejs");
+
+// use res.render to load up an ejs view file
+// app.use((req, res, next) => {
+//   const now = new Date();
+//   const day = now.getDay();
+//   const hour = now.getHours();
+
+//   const isavailbale = day >= 1 && day <= 7 && hour >= 9 && hour < 17;
+//   if (isavailbale) {
+//     next();
+//   } else {
+//     res.status(500).render("pages/notfound");
+//   }
+// });
+// index page
+app.get("/", function (req, res) {
+  res.render("pages/index");
+});
+
+// about page
+app.get("/Service", function (req, res) {
+  res.render("pages/Service");
+});
+app.get("/Contact", function (req, res) {
+  res.render("pages/Contact");
+});
+const port = 8080;
+app.listen(port, () => console.log("8080 is the magic port"));
